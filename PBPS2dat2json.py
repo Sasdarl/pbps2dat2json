8389fb93bb25a2b4c6f21270f0bbb3fe06326bfe
Sasdarl <77890964+Sasdarl@users.noreply.github.com>
2023-12-01 02:19:30 -0500

    Additional comments and output name priority

--- PBPS2dat2json.py
+++ PBPS2dat2json.py
@@ -32,27 +32,27 @@ def wu32(value):
 def wuFloat(value):
     return struct.pack("<f", value)
 
-def xorString(array, key):
-    str = struct.pack("<31s",array)
-    return bytes(a ^ key for a in str)
+def xorString(str, key):
+    strbytes = struct.pack("<31s",str) # The array is a string
+    return bytes(a ^ key for a in strbytes) # Array of integers
 
-def decodeRender(value):
+def decodeRender(value): # Used for render flags
     result = []
-    result.append((value & 0x02)>>1)
+    result.append((value & 0x02)>>1) # These are all the ones I know are used
     result.append((value & 0x04)>>2)
     result.append((value & 0x20)>>5)
     result.append((value & 0x200)>>9)
-    return result
+    return result # Ten-bit integer
 
-def encodeRender(array):
+def encodeRender(array): # Used for render flags
     result = array[0]<<1
     result += array[1]<<2
     result += array[2]<<5
     result += array[3]<<9
-    return result
+    return result # Array of integers, either 0 or 1
 
-def find_DAT(buf):
-    MAGIC = bytearray((int(0x21), int(0x01), int(0xF0), int(0xFF)))
+def find_DAT(buf): # Used for BTR files. It's used on regular DAT files too but immediately finds its target
+    MAGIC = bytearray((int(0x21), int(0x01), int(0xF0), int(0xFF))) # We're just searching for this
     offsets = []
     
     off_start = 0x0
@@ -65,7 +65,7 @@ def find_DAT(buf):
         if offset == -1:
             exit = True
         else:
-            return offset
+            return offset # Integer
     
     return -1
 
@@ -73,9 +73,9 @@ def DATtoJSON(buf):
     offset = 0x0C
     emptydict = {}
     dict = {}
-    dict["textures"] = []
+    dict["textures"] = [] # Python's JSON module is a godsend
     dict["groups"] = []
-    for i in range(ru32(buf,0x04)):
+    for i in range(ru32(buf,0x04)): # Textures
         texdict = {}
         texdict["width"] = ruFloat(buf,offset)
         texdict["height"] = ruFloat(buf,offset+4)
@@ -86,17 +86,17 @@ def DATtoJSON(buf):
         group["length"] = ruFloat(buf,offset)
         group["metadata"] = [0, 0, 0]
         for j in range(3):
-            group["metadata"][j] = ruFloat(buf,offset+4+j*4)
+            group["metadata"][j] = ruFloat(buf,offset+4+j*4) # I don't know what these do but there they are
         offset += 0x10
         if ru32(buf,offset) >= 1:
             offset += 0x08
             group["voiceLines"] = []
             for j in range(ru32(buf,offset-4)):
-                voiceLine = {}
+                voiceLine = {} # Legitimately no idea why these had to be XOR-encrypted
                 voiceLine["voiceLength"] = ruFloat(buf,offset)
                 offset += 0x04
-                key = buf[offset]
-                voicename = xorString(buf[offset+1:offset+32],key).rstrip('\x00'.encode())
+                key = buf[offset] # The first character is the key
+                voicename = xorString(buf[offset+1:offset+32],key).rstrip('\x00'.encode()) # Trim the null bytes
                 voiceLine["filename"] = chr(key) + voicename.decode()
                 offset += 0x20
                 group["voiceLines"].append(voiceLine)
@@ -106,7 +106,7 @@ def DATtoJSON(buf):
         offset += 0x04
         for j in range(ru32(buf,offset-4)):
             overlay = {}
-            overlay["renderFlags"] = decodeRender(ru32(buf,offset))
+            overlay["renderFlags"] = decodeRender(ru32(buf,offset)) # Special protocol
             overlay["textureNumber"] = ru32(buf,offset+4)
             overlay["xNumerator"] = ruFloat(buf,offset+8)
             overlay["yNumerator"] = ruFloat(buf,offset+12)
@@ -117,7 +117,7 @@ def DATtoJSON(buf):
             overlay["angle"] = ruFloat(buf,offset+32)
             offset += 40
 
-            overlay["vertexColor"] = [[],[],[],[]]
+            overlay["vertexColor"] = [[],[],[],[]] # Double array, why not
             for k in range(4):
                 overlay["vertexColor"][k] = [0, 0, 0, 0]
                 for l in range(4):
@@ -137,18 +137,18 @@ def DATtoJSON(buf):
             overlay["antiAliasing"] = ru32(buf,offset+4)
             offset += 8
 
-            if ru32(buf,offset) >= 1:
+            if ru32(buf,offset) >= 1: # Effects
                 overlay["effects"] = []
                 offset += 4
                 for k in range(ru32(buf,offset-4)):
                     effect = {}
-                    effect["effectType"] = ru32(buf,offset)
+                    effect["effectType"] = ru32(buf,offset) # Mess around and learn what for
                     if ru32(buf,offset+4) >= 1:
                         effect["effectKeyframes"] = []
                         offset += 12
                         for l in range(ru32(buf,offset-4)):
                             keyframe = []
-                            keyValueCount = ru32(buf,offset+4)
+                            keyValueCount = ru32(buf,offset+4) # This is set by keyframe in the file itself (why?)
                             keyframe.append(ruFloat(buf,offset))
                             for m in range(keyValueCount):
                                 keyframe.append(ruFloat(buf,offset+8+m*4))
@@ -156,24 +156,24 @@ def DATtoJSON(buf):
                             effect["effectKeyframes"].append(keyframe)
                     else:
                         offset += 8
-                    overlay["effects"].append(effect)
+                    overlay["effects"].append(effect) # Chain everything together
             else:
                 offset += 4
 
             group["overlays"].append(overlay)
         dict["groups"].append(group)
 
-    return json.dumps(dict, indent=userIndent)
+    return json.dumps(dict, indent=userIndent) # And in one stroke, we have a formatted string
 
 def JSONtoDAT(buf):
-    data = json.loads(buf)
+    data = json.loads(buf) # Load the dictionary
     NewDAT = []
-    if "headerMagic" not in data or data["headerMagic"] != 0:
-        NewDAT.append(wu32(0xFFF00121))
+    if "headerMagic" not in data or data["headerMagic"] != 0: # Compatibility with prerelease versions.
+        NewDAT.append(wu32(0xFFF00121)) # I don't recall if any examples of magic-less DAT files actually exist
     NewDAT.append(wu32(len(data["textures"])))
     NewDAT.append(wu32(len(data["groups"])))
 
-    for texture in data["textures"]:
+    for texture in data["textures"]: # Textures
         NewDAT.append(wuFloat(texture["width"]))
         NewDAT.append(wuFloat(texture["height"]))
     
@@ -184,10 +184,10 @@ def JSONtoDAT(buf):
 
         if "voiceLines" in group:
             NewDAT.append(wu32(1))
-            NewDAT.append(wu32(len(group["voiceLines"])))
+            NewDAT.append(wu32(len(group["voiceLines"]))) # Voice lines
             for line in group["voiceLines"]:
                 NewDAT.append(wuFloat(line["voiceLength"]))
-                voiceline = line["filename"][1::].encode()
+                voiceline = line["filename"][1::].encode() # Make this a string so we can reuse the function
                 key = ord(line["filename"][0])
                 NewDAT.append(wu08(key))
                 NewDAT.append(xorString(voiceline,key))
@@ -196,7 +196,7 @@ def JSONtoDAT(buf):
 
         NewDAT.append(wu32(len(group["overlays"])))
         for overlay in group["overlays"]:
-            NewDAT.append(wu32(encodeRender(overlay["renderFlags"])))
+            NewDAT.append(wu32(encodeRender(overlay["renderFlags"]))) # Render flags again
             NewDAT.append(wu32(overlay["textureNumber"]))
             NewDAT.append(wuFloat(overlay["xNumerator"]))
             NewDAT.append(wuFloat(overlay["yNumerator"]))
@@ -208,7 +208,7 @@ def JSONtoDAT(buf):
             NewDAT.append(wu32(group["overlays"].index(overlay)+1))
 
             for vc in overlay["vertexColor"]:
-                NewDAT.append(wuFloat(vc[0]))
+                NewDAT.append(wuFloat(vc[0])) # This probably barely edges out a for loop
                 NewDAT.append(wuFloat(vc[1]))
                 NewDAT.append(wuFloat(vc[2]))
                 NewDAT.append(wuFloat(vc[3]))
@@ -220,11 +220,11 @@ def JSONtoDAT(buf):
             NewDAT.append(wu32(overlay["brightnessMask"]))
             NewDAT.append(wu32(overlay["antiAliasing"]))
 
-            if "effects" in overlay:
+            if "effects" in overlay: # If there aren't effects, there are not effects
                 NewDAT.append(wu32(len(overlay["effects"])))
                 for effect in overlay["effects"]:
                     NewDAT.append(wu32(effect["effectType"]))
-                    if "effectKeyframes" in effect:
+                    if "effectKeyframes" in effect: # If there aren't keyframes, lockpicks won't work
                         NewDAT.append(wu32(1))
                         NewDAT.append(wu32(len(effect["effectKeyframes"])))
                         for keyframe in effect["effectKeyframes"]:
@@ -236,11 +236,11 @@ def JSONtoDAT(buf):
                         NewDAT.append(wu32(0))
             else:
                 NewDAT.append(wu32(0))
-    return NewDAT
+    return NewDAT # This is an array of 0-256 integers
     
 
-parser = argparse.ArgumentParser(description='Phantom Blood PS2 DAT-JSON Converter')
-parser.add_argument("inpath", help="File Input (DAT/JSON)")
+parser = argparse.ArgumentParser(description='Phantom Blood PS2 DAT-JSON Converter\nProgram by Hudgyn Sasdarl') # Technically they're LXE files
+parser.add_argument("inpath", help="File Input (DAT/JSON)") # but QuickBMS's assumed name for them stuck
 parser.add_argument("-o", "--outpath", help="Optional. The filename used for the output JSON/DAT file.")
 parser.add_argument("-sf", "--sourcefile", help="Optional. The file the resulting DAT should be written into when converting JSON. No effect without the source offset.")
 parser.add_argument("-so", "--sourceoffset", help="The offset in the source file the DAT should be written into when converting JSON. No effect without the source file.")
@@ -250,7 +250,7 @@ args = parser.parse_args()
 userIndent = 4
 if args.indent:
     userIndent = args.indent
-if not args.inpath.endswith(".json"): #DAT input is assumed
+if not args.inpath.endswith(".json"): # DAT input is assumed
     def save_json(input_file_buffer, output_json, outpath):
         with open(outpath, "wb") as outfile:
             outfile.write(output_json)
@@ -258,24 +258,24 @@ if not args.inpath.endswith(".json"): #DAT input is assumed
     
     with open(args.inpath, "rb") as input_file:
         input_file_buffer = bytearray( input_file.read() )
-        dat_offset = find_DAT(input_file_buffer)
+        dat_offset = find_DAT(input_file_buffer) # Let's just handle both regular DAT and BTR in one swoop
             
         dat = input_file_buffer[dat_offset:]
         output_json = DATtoJSON(dat)
         if args.outpath:
-            if dat_offset == 0:
+            if dat_offset == 0: # Add hexadecimal offset to aid rebuilding BTRs
                 outpath = (f"{Path(args.outpath).stem}{Path(args.outpath).suffix}")
             else:
                 outpath = (f"{Path(args.outpath).stem}_{hex(dat_offset)}{Path(args.outpath).suffix}")
         else:
-            if dat_offset == 0:
+            if dat_offset == 0: # Adding the "_output" part should keep the original file safe
                 outpath = (f"{Path(args.inpath).stem}_output.json")
             else:
                 outpath = (f"{Path(args.inpath).stem}_{hex(dat_offset)}_output.json")
         save_json(input_file_buffer, bytearray(output_json,"UTF-8"), outpath)
 
 
-elif args.inpath.endswith(".json"): #DAT output is assumed
+elif args.inpath.endswith(".json"): # DAT output is assumed
     with open(args.inpath, "rb") as input_file:
         input_file_buffer = bytearray( input_file.read() )
         output_dat = JSONtoDAT(input_file_buffer)
@@ -285,18 +285,19 @@ elif args.inpath.endswith(".json"): #DAT output is assumed
             with open(outpath, "wb") as outfile:
                 for byte in output_dat:
                     outfile.write(byte)
-        elif args.sourcefile and args.sourceoffset:
+        if args.sourcefile and args.sourceoffset: # Reinsertion, particularly into BTR
             outpath = (f"{Path(args.sourcefile).stem}_output{Path(args.sourcefile).suffix}")
-            if args.sourceoffset[1] == "x":
-                sourceOffset = int(args.sourceoffset,16)
-            else:
-                sourceOffset = int(args.sourceoffset,10)
-            shutil.copy(args.sourcefile,outpath)
+            if args.outpath:
+                outpath = (f"{Path(args.outpath)}") # User output choice takes priority
+            sourceOffset = int(args.sourceoffset,16) # But not in decimal
+            shutil.copy(args.sourcefile,outpath) # This is probably faster
             with open(outpath, "r+b") as outfile:
                 outfile.seek(sourceOffset)
                 for byte in output_dat:
                     outfile.write(byte)
         else:
+            if args.outpath:
+                outpath = (f"{Path(args.outpath)}")
             with open(outpath, "wb") as outfile:
                 for byte in output_dat:
                     outfile.write(byte)
